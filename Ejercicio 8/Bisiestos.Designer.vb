88053c67e69f3b82e6a2821e863b835b2ade3c41
Juan Freille <juanfreille@gmail.com>
2019-09-14 21:38:22 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio 8/Bisiestos.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtDia = New System.Windows.Forms.TextBox()
+        Me.txtMes = New System.Windows.Forms.TextBox()
+        Me.txtAño = New System.Windows.Forms.TextBox()
+        Me.cmdValidar = New System.Windows.Forms.Button()
+        Me.LblResultado = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(12, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(238, 57)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ingrese día, mes y año de una fecha para determinar si se trata de una fecha vali" &
+    "da o no."
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 136)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(25, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Día"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 168)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(27, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Mes"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(15, 203)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(26, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Año"
+        '
+        'txtDia
+        '
+        Me.txtDia.Location = New System.Drawing.Point(65, 136)
+        Me.txtDia.Name = "txtDia"
+        Me.txtDia.Size = New System.Drawing.Size(100, 20)
+        Me.txtDia.TabIndex = 4
+        '
+        'txtMes
+        '
+        Me.txtMes.Location = New System.Drawing.Point(65, 165)
+        Me.txtMes.Name = "txtMes"
+        Me.txtMes.Size = New System.Drawing.Size(100, 20)
+        Me.txtMes.TabIndex = 5
+        '
+        'txtAño
+        '
+        Me.txtAño.Location = New System.Drawing.Point(65, 200)
+        Me.txtAño.Name = "txtAño"
+        Me.txtAño.Size = New System.Drawing.Size(100, 20)
+        Me.txtAño.TabIndex = 6
+        '
+        'cmdValidar
+        '
+        Me.cmdValidar.Location = New System.Drawing.Point(171, 198)
+        Me.cmdValidar.Name = "cmdValidar"
+        Me.cmdValidar.Size = New System.Drawing.Size(89, 23)
+        Me.cmdValidar.TabIndex = 7
+        Me.cmdValidar.Text = "Validar"
+        Me.cmdValidar.UseVisualStyleBackColor = True
+        '
+        'LblResultado
+        '
+        Me.LblResultado.AutoSize = True
+        Me.LblResultado.Location = New System.Drawing.Point(15, 266)
+        Me.LblResultado.Name = "LblResultado"
+        Me.LblResultado.Size = New System.Drawing.Size(118, 13)
+        Me.LblResultado.TabIndex = 9
+        Me.LblResultado.Text = "Resultado:  _________"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(272, 345)
+        Me.Controls.Add(Me.LblResultado)
+        Me.Controls.Add(Me.cmdValidar)
+        Me.Controls.Add(Me.txtAño)
+        Me.Controls.Add(Me.txtMes)
+        Me.Controls.Add(Me.txtDia)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtDia As TextBox
+    Friend WithEvents txtMes As TextBox
+    Friend WithEvents txtAño As TextBox
+    Friend WithEvents cmdValidar As Button
+    Friend WithEvents LblResultado As Label
+End Class
---
+++ Ejercicio 8/Bisiestos.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+
+    Private Sub cmdValidar_Click(sender As Object, e As EventArgs) Handles cmdValidar.Click
+        Dim D As Integer = 0
+        Dim M As Integer = 0
+        Dim A As Integer = 0
+
+        If FechaValida(txtDia, txtMes, txtAño) = True Then
+            LblResultado.Text = "Resultado: FECHA VALIDA"
+        Else
+            LblResultado.Text = "Resultado: FECHA INVALIDA!"
+        End If
+    End Sub
+
+    Function FechaValida(D, M, A)
+        Dim Valida As Boolean = True 'Por defecto, se considera que es valida
+        D = Val(txtDia.Text)
+        M = Val(txtMes.Text)
+        A = Val(txtAño.Text)
+        If M < 1 Or M > 12 Then
+            Valida = False
+        Else
+            If (M = 1 Or M = 3 Or M = 5 Or M = 7 Or M = 8 Or M = 10 Or M = 12) And (D < 1 Or D > 31) Then
+                Valida = False
+            Else
+                If (M = 4 Or M = 6 Or M = 9 Or M = 11) And (D < 1 Or D > 30) Then
+                    Valida = False
+                Else
+                    If M = 2 And Bisiesto(A) = True And (D < 1 Or D > 29) Then
+                        Valida = False
+                    Else
+                        If M = 2 And Bisiesto(A) = False And (D < 1 Or D > 28) Then
+                            Valida = False
+                        End If
+                    End If
+                End If
+            End If
+        End If
+        Return Valida
+    End Function
+
+    Function Bisiesto(A)
+        Dim EsBisiesto As Boolean = False  'Por defecto, se considera que no es bisiesto 
+
+        If A Mod 4 = 0 Then
+            If A Mod 100 = 0 Then
+                If A Mod 400 = 0 Then
+                    EsBisiesto = True
+                End If
+            Else
+                EsBisiesto = True
+            End If
+        End If
+        Return EsBisiesto
+    End Function
+
+End Class
